b78eb41ce232d88e8da6b018267be475384e2f44
H1DDENADM1N <65646535+H1DDENADM1N@users.noreply.github.com>
2025-10-24 14:19:32 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,14 +1,14 @@
 
-@REM conda activate py312
+@REM conda activate py313
 
 where cmake
 @REM C:\Program Files\CMake\bin\cmake.exe
 
 where gcc
-@REM C:\mingw64\bin\gcc.exe
+@REM C:\msys64\mingw64\bin\gcc.exe
 
 python --version
-@REM Python 3.12.4
+@REM Python 3.13.9
 
 cmake -G "MinGW Makefiles" .
 cmake --build .
